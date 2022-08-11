10621e32d7a9ed1604d607224ec65cbedb4d9150
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-08-11 14:59:25 +0200

    import des données ok, et suelette html ok

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
-<!-- J'importe mes données -->
-<?php include_once('db.php'); ?>
+<!-- Création de la ressource curl -->
+<?php 
 
-<!-- Créattion de la ressource curl -->
-<?php $curl = curl_init(); ?>
+$url = "http://localhost:3009/avis";
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$listAvis = curl_exec($ch);
+curl_close($ch);
+$responses = json_decode($listAvis, true);
+var_dump($responses);
+?>
 
 <!-- Je boucle sur mes données -->
-<?php foreach (($avis) as $avis) : ?>
+<?php foreach (($responses) as $response) : ?>
     <article>
-        <h3><?php echo $avis['pseudo']; ?></h3>
-        <p>Note <?php echo $avis['rating']; ?></p>
-        <p> Commentaire <?php echo $avis['comment']; ?></p>
-        <p><?php echo $avis['created_at']; ?></p>
+        <h3><?php echo $response['pseudo']; ?></h3>
+        <p>Note <?php echo $response['rating']; ?></p>
+        <p> Commentaire <?php echo $response['comment']; ?></p>
+        <p><?php echo $response['created_at']; ?></p>
     </article>
 <?php endforeach ?>
