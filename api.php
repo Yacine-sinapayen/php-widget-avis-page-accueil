9b4c49fa0dd51a76212a0a2860c89556247287bd
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-08-24 09:48:43 +0200

    final version widget

--- api.php
+++ api.php
@@ -1,37 +1,26 @@
-<!-- Je récupère le nom de la formation depuis l'url -->
-<!-- php 
-$url = "https://testurl.com/test/1234?formation=facette&name=sarah";
-$components = parse_url($url);
-parse_str($components['query'], $results);
-echo($results['formation']); 
+<?php 
+// Je récupère le nom de la formation depuis l'url courant de la page
 
-ou bien 
-echo $_GET['formation']
--->
+$nameCourse = isset($_GET['formation']) ? $_GET['formation'] : '' ;
 
-<!-- Je récupère les avis de ma formation depuis mon api -->
-<?php
-$url = "http://localhost:3009/avis";
+
+// var_dump($nameCourse);
+
+// Je récupère les avis de ma formation depuis mon api
+$api = "https://formations.learnylib.com/api/v1/ws/courses/{$nameCourse}/reviews";
 $ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_URL, $api);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($ch);
 curl_close($ch);
 $responses = json_decode($data, true);
 
-
 //----- Je ne récupère que les notes au dessus de 4 -----//
 $goodValues = array_filter($responses, function ($e) {
     return $e['rating'] >= "4";
-    //Use this to be sure
-    //return strtolower($e['type']) == "good";
 });
 
 //----- Parmis ces notes au dessus de 4 je ne veux que les 10 dernières -----//
 $new_array = array_slice($goodValues, 0, 10);
 
-// print_r( $new_array);
-// echo json_encode($goodValues);
-// var_dump($goodValues)
 ?>
