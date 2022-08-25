eec39af0f224a52d191c8fb837aeae401a45761d
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-08-25 23:29:26 +0200

    push avant supression en local

--- api.php
+++ api.php
@@ -3,25 +3,20 @@
 
 $nameCourse = isset($_GET['formation']) ? $_GET['formation'] : '' ;
 
-
-// var_dump($nameCourse);
-
 // Je récupère les avis de ma formation depuis mon api
-
-
 
 // ----- Test affichage formation avec un seul avis -----//
 //$api = "https://formations.learnylib.com/api/v1/ws/courses/recrutement/reviews";
 
 
 // ----- Test affichage formation avec un deux avis -----//
-$api = "https://formations.learnylib.com/api/v1/ws/courses/anesthesie-dentaire/reviews";
+// $api = "https://formations.learnylib.com/api/v1/ws/courses/anesthesie-dentaire/reviews";
 
 //----- Test affichage formation avec plusieurs avis -----//
 // $api = "https://formations.learnylib.com/api/v1/ws/courses/facettes/reviews";
 
 //----- BONNE VERSION DE L'URL EN PROD -----//
-// $api = "https://formations.learnylib.com/api/v1/ws/courses/{$nameCourse}/reviews";
+$api = "https://formations.learnylib.com/api/v1/ws/courses/{$nameCourse}/reviews";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $api);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
